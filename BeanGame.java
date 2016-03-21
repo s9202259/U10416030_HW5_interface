0b97e682d8a47e8c30820bd0fe14885779f942fd
s9202259 <s9202259@yahoo.com.tw>
2016-03-21 15:48:00 +0800

    稍做修改
    新增paint來畫圓
    新增label

--- BeanGame.java
+++ BeanGame.java
@@ -4,26 +4,34 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
 
-public class BeanGame {
-	JFrame frame;
-	JMenuItem item1, item2, item3;
-	JPopupMenu popupMenu;
-	
-	public static void main(String[] args) {
-		new BeanGame();
-	}
+public class BeanGame extends JFrame{
+	static JFrame myFrame = new BeanGame();
 	
 	public BeanGame() {
-		frame = new JFrame("BeanGame");
-		frame.setSize(600, 400);
-		frame.setLayout(new GridLayout(6, 4));
+		JLabel label = new JLabel();
+		label.setIcon(new ImageIcon("C:/Image/BeanGame.png"));
+		label.setPreferredSize(new Dimension(330, 450));
+		paint(100, 200);
 		
-		JLabel label = new JLabel("BeanGame");
-		label.setIcon(new ImageIcon("C:/Image/1.JPG"));
+		this.add(label);
+		this.pack();
+	}
+	
+	public static void main(String[] args) {
+		myFrame.setSize(500, 500);
+		myFrame.setResizable(false);
+		myFrame.setVisible(true);
 		
-		frame.add(label);
-		frame.setVisible(true);
-		
-		System.out.println("getLabel:");
+		myFrame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+	
+	public static void paint(int x, int y) {
+		Graphics g = myFrame.getGraphics();
+		g.setColor(Color.red);
+		g.fillOval(x, y, 10, 10);
 	}
 }
